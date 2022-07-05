bfe6a0ba47883ed1a5a7333adfbeae63f53c4a1d
TackRegnar <TackRegnar@protonmail.com>
2022-07-05 01:48:03 -0500

    basic functions for math

---
+++ script.js
@@ -0,0 +1,15 @@
+function adder(num1, num2) {
+  return num1 + num2;
+}
+
+function subtracter(num1, num2) {
+  return num1 - num2;
+}
+
+function multiplyer(num1, num2) {
+  return num1 * num2;
+}
+
+function divider(num1, num2) {
+  return num1 / num2;
+}
